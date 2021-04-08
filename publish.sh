b643b05d62d9fbbbf2d4b244b97f28afad025878
漆家佳 <lanmemory@163.com>
2021-04-08 14:13:16 +0800

    0.1.2

--- publish.sh
+++ publish.sh
@@ -75,7 +75,7 @@ remoteVerifyLib(){
 #发布库
 publishLib(){
     echo -e "${GREEN}\n第六步：准备发布${tag}版本${NC}⏰⏰⏰"
-    if ! pod trunk push ${SpecName} --allow-warnings --sources="${source1}" --allow-warnings --use-libraries --skip-import-validation; then echo -e "${RED}发布${tag}版本失败${NC}🌧🌧🌧"; exit 1; fi
+    if ! pod trunk push ${SpecName} --allow-warnings; then echo -e "${RED}发布${tag}版本失败${NC}🌧🌧🌧"; exit 1; fi
     echo -e "${GREEN}发布${tag}版本成功${NC}🚀🚀🚀"
 }
 
